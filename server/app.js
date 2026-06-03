ada80fc93b79e6de1cac96e0b58646c54a96ce2d
Akanksha Shukla <akanskashukla1611@gmail.com>
2026-06-03 20:20:32 +0530

    fix cors for vercel deployment

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: (process.env.CLIENT_URL || 'http://localhost:3000').split(','),
+    origin: true,
     credentials: true,
   })
 );
